e931d22ae4f4c6cac96f7acbe86b971f0a98f02a
Theporza <theporza55yo@gmail.com>
2026-07-10 21:22:28 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -422,4 +422,5 @@ def topup_truemoney():
         return jsonify({"message": f"ไม่สามารถติดต่อระบบ TrueMoney ได้: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
